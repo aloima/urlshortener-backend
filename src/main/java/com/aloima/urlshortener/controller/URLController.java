f22013030896302eff3c7f323dee781262277052
aloima <acar.aloima@gmail.com>
2025-05-26 17:47:06 +0300

    fix: invalid response from DELETE method

--- src/main/java/com/aloima/urlshortener/controller/URLController.java
+++ src/main/java/com/aloima/urlshortener/controller/URLController.java
@@ -5,12 +5,12 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,7 +42,8 @@ public class URLController {
         urlService.saveURL(data);
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, path = "/{id}", produces = "application/json")
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteURL(@PathVariable String id) {
         boolean response = urlService.deleteURL(id);
         if (!response) throw new ResourceNotFoundException("URL with id '" + id + "' cannot be found, so it cannot be deleted.");
