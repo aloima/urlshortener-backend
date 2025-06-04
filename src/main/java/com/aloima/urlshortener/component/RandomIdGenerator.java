370b132525d0818974eb182037cd92382c8d18a9
aloima <acar.aloima@gmail.com>
2025-06-04 15:33:36 +0300

    refactor: remove some homoglyphs from random ids

--- src/main/java/com/aloima/urlshortener/component/RandomIdGenerator.java
+++ src/main/java/com/aloima/urlshortener/component/RandomIdGenerator.java
@@ -9,29 +9,29 @@ public class RandomIdGenerator {
     public final SecureRandom random = new SecureRandom();
 
     public long generateRandomId() {
-        return this.random.nextLong(1, (long) Math.pow(62, 7));
+        return this.random.nextLong(1, (long) Math.pow(59, 7));
     }
 
     public String idToString(long id) {
-        String charset = "012345689abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String charset = "12345689abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ"; // without 0, l, I
         StringBuilder result = new StringBuilder("");
 
         while (id > 0) {
-            result.append(charset.charAt(((int) Long.remainderUnsigned(id, 62))));
-            id /= 62;
+            result.append(charset.charAt(((int) Long.remainderUnsigned(id, 59))));
+            id /= 59;
         }
 
         return result.toString();
     }
 
     public long stringToId(String value) {
-        String charset = "012345689abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String charset = "12345689abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ"; // without 0, l, I
         long id = 0;
 
         int length = value.length();
 
         for (int i = 0; i < length; ++i) {
-            id += (charset.indexOf(value.charAt(i)) * Math.pow(62, i));
+            id += (charset.indexOf(value.charAt(i)) * Math.pow(59, i));
         }
 
         return id;
