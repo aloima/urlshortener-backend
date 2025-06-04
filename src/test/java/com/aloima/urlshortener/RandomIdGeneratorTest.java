c1892d1b984a482ed4137d3875658e7775b2be36
aloima <acar.aloima@gmail.com>
2025-06-04 16:10:08 +0300

    test: add @BeforeEach to RandomIdGenerator

--- src/test/java/com/aloima/urlshortener/RandomIdGeneratorTest.java
+++ src/test/java/com/aloima/urlshortener/RandomIdGeneratorTest.java
@@ -4,37 +4,40 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.aloima.urlshortener.component.RandomIdGenerator;
 
 class RandomIdGeneratorTest {
+    private RandomIdGenerator generator;
+
+    @BeforeEach
+    void createRandomIdGenerator() {
+        generator = new RandomIdGenerator();
+        assertNotNull(generator);
+    }
+
     @Test
     void generateValidValue() throws Exception {
-        RandomIdGenerator random = new RandomIdGenerator();
-        assertNotNull(random);
-
-        long id = random.generateRandomId();
+        long id = generator.generateRandomId();
         assertNotNull(id);
 
         assertTrue(1 <= id);
-        assertTrue(id < Math.pow(62, 7));
+        assertTrue(id < Math.pow(59, 7));
     }
 
     @Test
     void checkValidConversions() throws Exception {
-        RandomIdGenerator random = new RandomIdGenerator();
-        assertNotNull(random);
-
-        long generated = random.generateRandomId();
-        String value = random.idToString(generated);
+        long generated = generator.generateRandomId();
+        String value = generator.idToString(generated);
         assertNotNull(value);
 
         assertTrue(value.matches("[A-Za-z0-9]+"));
         assertTrue(1 <= value.length());
         assertTrue(value.length() <= 7);
 
-        long id = random.stringToId(value);
+        long id = generator.stringToId(value);
         assertNotNull(id);
         assertEquals(id, generated);
     }
